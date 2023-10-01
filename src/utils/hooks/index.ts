dcd07007e5e6fcd4bc5b5170820dbed270136ed8
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-10-01 17:13:44 -0500

    feat: add hook useupdate
    - Add hook useUpdate for objects state with partial changes

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from "./createUseContext";
+export * from "./useUpdate";
---
+++ src/utils/hooks/useUpdate.ts
@@ -0,0 +1,12 @@
+import { useReducer } from "react";
+
+const useUpdate = <T extends object>(
+  initialState: T
+): [T, (update: Partial<T>) => void] => {
+  return useReducer(
+    (prev: T, action: Partial<T>) => ({ ...prev, ...action }),
+    initialState
+  );
+};
+
+export { useUpdate };
